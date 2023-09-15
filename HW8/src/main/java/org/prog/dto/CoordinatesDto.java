510fcc2c2604ee0460fb69688d8f9529f6e2e26d
Maksmvs <142376726+Maksmvs@users.noreply.github.com>
2023-09-15 10:45:14 +0300

    Add files via upload

---
+++ HW8/src/main/java/org/prog/dto/CoordinatesDto.java
@@ -0,0 +1,17 @@
+package org.prog.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CoordinatesDto {
+
+    private String latitude;
+    private String longitude;
+
+}
